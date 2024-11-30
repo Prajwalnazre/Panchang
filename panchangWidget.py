f2c4b7e067465b0f0ec2213b7d503d06b840941a
PrajwalNazre <prajwal.nazre777@gmail.com>
2024-11-30 23:31:36 +0530

    Fixed close/minimize button issue; Added assets for future use of icon

--- panchangWidget.py
+++ panchangWidget.py
@@ -1,19 +1,22 @@
 import tkinter as tk
 
 root = tk.Tk()
+# root.geometry('300x200')
+root.resizable(False, False)
+root.overrideredirect(False)
+
+close_icon = tk.PhotoImage(file='./Assets/x_icon_2.png')
+minimize_icon = tk.PhotoImage(file='./Assets/minimize_icon_background.png')
 
 def close_panchang_window() :
     root.destroy()
 
 def panchang_window() :
     root.title("Panchang")
-    root.overrideredirect(True)
+    # root.overrideredirect(True)
     root.wm_attributes("-topmost", True)
 
-    label = tk.Label(root, text="Panchang Here", font=("Arial", 24))
-    label.pack(padx=20, pady=20)
-
-    close_button = tk.Button(root, text="Close", command=close_panchang_window, font=("Arial", 12))
-    close_button.pack(pady=10)
+    label = tk.Label(root, text="Panchang Here", font=("Arial", 15))
+    label.pack(padx=10, pady=10)
 
     root.mainloop()
