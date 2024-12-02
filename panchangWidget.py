88412aa461fa1bd377a1d0681ada7ef4b61d493e
PrajwalNazre <prajwal.nazre777@gmail.com>
2024-12-02 22:35:15 +0530

    Global variable for font added

--- panchangWidget.py
+++ panchangWidget.py
@@ -12,6 +12,9 @@ today = date.today()
 akasha_font_path = 'AkashaRegular-Rprn6.ttf'
 akasha_font_pil_font = ImageFont.truetype(akasha_font_path, size=17)
 akasha_font = Font(family=akasha_font_pil_font.font.family, size=17)
+
+final_font = akasha_font
+
 # close_icon = tk.PhotoImage(file='./Assets/x_icon_2.png')
 # minimize_icon = tk.PhotoImage(file='./Assets/minimize_icon_background.png')
 
@@ -22,10 +25,10 @@ def panchang_window(panchang_object) :
     root.title("Panchang")
     # root.overrideredirect(True)
     # root.wm_attributes("-topmost", True)
-    root.configure(bg="lightblue")
+    root.configure(bg="#fdb563")
     today_upper_case = str(today.strftime('%B %d, %Y'))
     print(akasha_font)
-    main_label = tk.Label(root, bg = "lightyellow", text=f"TODAY - {today_upper_case.upper()}", font=akasha_font)
+    main_label = tk.Label(root, bg = "lightyellow", text=f"TODAY - {today_upper_case.upper()}", font=final_font)
     main_label.grid(row=0, columnspan=2, padx=10, pady=8, sticky="nsew")
     panchang_object.printPanchangInfo()
     label_texts = [
@@ -35,10 +38,10 @@ def panchang_window(panchang_object) :
         f"DAY OF THE MONTH : {panchang_object.dayOfMonth}"
     ]
 
-    maasa_label = tk.Label(root, text = label_texts[0], bg="lightblue", font=akasha_font)
-    thithi_label = tk.Label(root, text=label_texts[1], bg="lightblue", font=akasha_font)
-    paksha_label = tk.Label(root, text=label_texts[2], bg="lightblue", font=akasha_font)
-    day_of_the_month_label = tk.Label(root, text=label_texts[3], bg="lightblue", font=akasha_font)
+    maasa_label = tk.Label(root, text = label_texts[0], bg="#fdb563", font=final_font)
+    thithi_label = tk.Label(root, text=label_texts[1], bg="#fdb563", font=final_font)
+    paksha_label = tk.Label(root, text=label_texts[2], bg="#fdb563", font=final_font)
+    day_of_the_month_label = tk.Label(root, text=label_texts[3], bg="#fdb563", font=final_font)
 
     maasa_label.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
     day_of_the_month_label.grid(row=1, column=1,padx=10, pady=5, sticky=tk.W)
